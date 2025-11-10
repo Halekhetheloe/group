1b62ac6b5715636f7ca8ee94105b2699977e6c55
mokhothu934 <mokhothuhalekhetheloe@gmail.com>
2025-11-10 19:52:29 +0200

    refactor: remove sidebar and update sitemap
    - Remove sidebar from Layout component for cleaner design
    - Update Sitemap page with correct routes and remove icons
    - Ensure all navigation links work properly
    - Simplify layout to navbar-only structure

--- client/src/components/shared/Layout/Layout.jsx
+++ client/src/components/shared/Layout/Layout.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { useAuth } from '../../../hooks/useAuth'
 import Navbar from './Navbar'
-import Sidebar from './Sidebar'
 import Footer from './Footer'
 
 const Layout = ({ children }) => {
@@ -31,21 +30,12 @@ const Layout = ({ children }) => {
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Navbar />
       
-      <div className="flex-1 flex">
-        {/* Sidebar - Only show for authenticated users in dashboard sections */}
-        {userData && isDashboard && (
-          <div className="hidden lg:block">
-            <Sidebar userRole={userData.role} />
-          </div>
-        )}
-        
-        {/* Main Content */}
-        <main className={`flex-1 ${userData && isDashboard ? 'lg:ml-0' : ''}`}>
-          <div className={`${userData && isDashboard ? 'p-6' : ''}`}>
-            {children}
-          </div>
-        </main>
-      </div>
+      {/* Main Content - Full width without sidebar */}
+      <main className="flex-1 pt-16"> {/* Added padding-top for fixed navbar */}
+        <div className={userData && isDashboard ? 'p-6' : ''}>
+          {children}
+        </div>
+      </main>
       
       <Footer />
     </div>
